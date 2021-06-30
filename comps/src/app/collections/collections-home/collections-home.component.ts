f07952b93fc4e22d8b0649f46373fe0901462652
Jeanie Herold <jeaniemherold68@gmail.com>
2021-06-30 18:15:58 -0400

    table component complete

--- comps/src/app/collections/collections-home/collections-home.component.ts
+++ comps/src/app/collections/collections-home/collections-home.component.ts
@@ -6,12 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  //data & headers configuration array of objects for our Table Component
+  //adding extra key/values in the data set to show how our header config
+  //defines what we actually see in the table
   data = [
-    { name: 'Willie', age: 24, job: 'Designer'},
-    { name: 'Lexi', age: 34, job: 'Engineer'},
-    { name: 'Jeanie', age: 44, job: 'Engineer'}
+    { name: 'Willie', age: 24, job: 'Designer', employed: false },
+    { name: 'Lexi', age: 34, job: 'Engineer', employed: false },
+    { name: 'Jeanie', age: 44, job: 'Engineer', employed: true }
   ];
 
+  //data.employed wont show in table unless you add the key: ''employed' 
+  //to the header configuration
+  //we only need to change these configuration properties to add additional data
+  //dont need to change the actual Table Component markup
   headers = [
     { key: 'name', label: 'Name'},
     { key: 'age', label: 'Age'},
--- comps/src/app/collections/table/table.component.ts
+++ comps/src/app/collections/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  @Input() classNames: string = '';
+  @Input() data: Array<any> = [];
+  @Input() headers: Array<any> = [];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
