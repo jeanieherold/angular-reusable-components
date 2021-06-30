24e1f67615724c01b4840fa7675b3aae111fb03b
Jeanie Herold <jeaniemherold68@gmail.com>
2021-06-30 13:14:35 -0400

    saving through getting started with tables

--- comps/src/app/collections/collections-home/collections-home.component.ts
+++ comps/src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'Willie', age: 24, job: 'Designer'},
+    { name: 'Lexi', age: 34, job: 'Engineer'},
+    { name: 'Jeanie', age: 44, job: 'Engineer'}
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name'},
+    { key: 'age', label: 'Age'},
+    { key: 'job', label: 'Job'}
+  ];
 
   constructor() { }
 
--- comps/src/app/collections/collections.module.ts
+++ comps/src/app/collections/collections.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { TableComponent } from './table/table.component';
 
 
 @NgModule({
   declarations: [
-    CollectionsHomeComponent
+    CollectionsHomeComponent,
+    TableComponent
   ],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ],
   //only need to export if being used directly in another module - we are using on collections path only
   exports: []
